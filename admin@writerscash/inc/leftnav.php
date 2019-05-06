8eb5073df9dd7eb769873dd1a5364266322a3dc7
9wilson6 <gatheruwilson@gmail.com>
2019-05-06 22:06:50 +0300

    admin navbar in prgress

--- admin@writerscash/inc/leftnav.php
+++ admin@writerscash/inc/leftnav.php
@@ -1,81 +1,29 @@
 <?php require_once "../inc/sessios.php";
 session_admin();
 ?>
-<section class="dashboard_nav_left">
-  <div class="dashboard_nav_left__content">
-    <ul class="">
-      <a href="index"  <?php if ($page=="dashboard") { ?>
-        class="active"
-        <?php } ?>><li class="">Dashboard <i class="fas fa-tachometer-alt icon-l"></i></li></a>
-
-        <a href="#"  <?php if ($mainpage=="student") { ?>
-          class="active" 
-          <?php } ?> data-toggle="collapse" data-target="#student">
-          <li class="list-group-item">Students Management
-            <i class="fas fa-angle-double-down icon-l"></i></li></a>
-            <div id="student" class="collapse">
-              <ul class="list-group">
-               <a href="stud_activate" <?php if ($page=="student_activate") { ?>
-                class="active" 
-                <?php } ?> ><li class="list-group-item">Activate <i class="fas fa-toggle-on icon-l"></i></li></a>
-
-                <a href="student_suspend" <?php if ($page=="student_suspend") { ?>
-                  class="active" 
-                  <?php } ?> ><li class="list-group-item">Suspend <i class="fas fa-power-off icon-l"></i></li></a>
-
-                  <a href="stud_message" <?php if ($page=="stud_message") { ?>
-                    class="active" 
-                    <?php } ?> ><li class="list-group-item">Message <i class="far fa-comments icon-l"></i></li></a>
-
-
-                  </ul>
-                </div>
-
-                <a href="#"  <?php if ($mainpage=="tutor") { ?>
-                  class="active" 
-                  <?php } ?> data-toggle="collapse" data-target="#tutor"><li class="list-group-item">Tutors Management<i class="fas fa-angle-double-down icon-l"></i> </li></a>
-
-                  <div id="tutor" class="collapse">
-
-                    <ul class="list-group">
-                     <a href="tut_activate" <?php if ($page=="tutor") { ?>
-                      class="active" 
-                      <?php } ?> ><li class="list-group-item">Activate <i class="fas fa-toggle-on icon-l"></i></li></a>
-
-                      <a href="tutor_suspend" <?php if ($page=="tut_suspend") { ?>
-                        class="active" 
-                        <?php } ?> ><li class="list-group-item">Suspend <i class="fas fa-power-off icon-l"></i></li></a>
-
-                        <a href="tut_message" <?php if ($page=="tut_message") { ?>
-                          class="active" 
-                          <?php } ?> ><li class="list-group-item">Message <i class="far fa-comments icon-l"></i></li></a>
-
-                        </ul>
-                      </div>
-
-
-                      <a href="#"  <?php if ($mainpage=="orders") { ?>
-                        class="active" 
-                        <?php } ?> data-toggle="collapse" data-target="#orders"><li class="list-group-item">Projects Management
-                         <i class="fas fa-angle-double-down icon-l"></i></li></a>
-                         <div id="orders" class="collapse">
-                          <ul class="list-group">
-                            <a href="manage_order" <?php if ($page=="manage_order") { ?>
-                              class="active" 
-                              <?php } ?> ><li class="list-group-item">Clean-Up<i class="far fa-trash-alt icon-l"></i></li></a>
-
-                            </ul>
-                          </div>
-
-
-
-                          <a href="../logout"> <li class="list-group-item"><i class="fas fa-sign-out-alt icon-r"></i>Logout</li></a>
-
-                        </ul>
-                      </div>
+                <section class="admin_left_nav">
+ <div class="nav_">
+        <ul>
+        
+        <li><a href="#">Home</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Feedback</a></li>
+        <li><a href="#">More</a>
+          <ul>
+          <li><a href="#">More 1</a></li>
+          <li><a href="#">More 2</a></li>
+          <li><a href="#">More 3</a></li>
+          <li><a href="#">More 4</a></li>
+          <li></li>
+          </ul>
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
                     </section>
-                    <?php #require_once("../inc/footer_links.php") ?>
-                    <!-- Left navigation bar -->
+                  
 
                     <script>
 
